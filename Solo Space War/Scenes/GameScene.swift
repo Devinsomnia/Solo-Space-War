5e4970863c9e7af94b00d2e8d4032d94a2af9c53
Tuncay Cansız <developinginsomnia@gmail.com>
2018-12-03 20:30:44 +0300

    Proje Düzenlemesi Yapıldı.
    İlk oluşturan sprite kit projesinde bulunan gereksiz sahneleri ve sahneler içerisinde ki gereksiz kodlar silinerek geliştireceğimiz projeye zemin hazırlanmıştır.

---
+++ Solo Space War/Scenes/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Solo Space War
+//
+//  Created by Tuncay Cansız on 3.12.2018.
+//  Copyright © 2018 Tuncay Cansız. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+}
---
+++ Solo Space War/Scenes/MainScene.swift
@@ -0,0 +1,15 @@
+//
+//  MainScene.swift
+//  Solo Space War
+//
+//  Created by Tuncay Cansız on 3.12.2018.
+//  Copyright © 2018 Tuncay Cansız. All rights reserved.
+//
+
+import SpriteKit
+
+class MainScene: SKScene{
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor.red
+    }
+}
